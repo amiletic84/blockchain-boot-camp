0aa473e632220c807e4b7a76f0b673af1131b935
Aleksandar Miletic <aleksandar.miletic@comtrade.com>
2021-11-25 23:59:56 +0100

    Updates

---
+++ precache-manifest.489b944e03e07a98ca320ee48ec46302.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "884b17eb594b2e8c2660b59828205b4f",
+    "url": "/blockchain-developer-bootcamp-final-project/index.html"
+  },
+  {
+    "revision": "c75cbfcc6153bf6418c8",
+    "url": "/blockchain-developer-bootcamp-final-project/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "09b4aa2adf26027d11ec",
+    "url": "/blockchain-developer-bootcamp-final-project/static/js/2.5bd92442.chunk.js"
+  },
+  {
+    "revision": "c75cbfcc6153bf6418c8",
+    "url": "/blockchain-developer-bootcamp-final-project/static/js/main.c50ec57e.chunk.js"
+  },
+  {
+    "revision": "974fe9a407c487a617cb",
+    "url": "/blockchain-developer-bootcamp-final-project/static/js/runtime-main.dca84915.js"
+  }
+]);
